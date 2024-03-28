e7fe01290376a901e14ff19b7c5d92e6fab7c21f
MahadevBiniraj <mahadevbiniraj6@gmail.com>
2024-03-28 19:58:08 +0530

    bottom changed

--- lib/view/note_screen/note_screen.dart
+++ lib/view/note_screen/note_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:notesapplication/view/note_screen/widgets/notecard.dart';
 
 class Notescreen extends StatefulWidget {
@@ -12,6 +15,59 @@ class _NotescreenState extends State<Notescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 15,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Add Note",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          fillColor: Color(0xffc0c0c0),
+                          filled: true,
+                          hintText: "Titile"),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          fillColor: Color(0xffc0c0c0),
+                          filled: true,
+                          hintText: "Description"),
+                      maxLines: 4,
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          fillColor: Color(0xffc0c0c0),
+                          filled: true,
+                          hintText: "Date"),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          child: Icon(Icons.add),
+        ),
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.black,
         body: ListView.builder(
