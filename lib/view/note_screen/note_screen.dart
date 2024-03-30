26c5e0325a241d9e771bf85a1fe2853266447d26
MahadevBiniraj <mahadevbiniraj6@gmail.com>
2024-03-30 20:03:31 +0530

    changes updated

--- lib/view/note_screen/note_screen.dart
+++ lib/view/note_screen/note_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:notesapplication/view/note_screen/widgets/notecard.dart';
 
@@ -20,80 +21,127 @@ class _NotescreenState extends State<Notescreen> {
             showModalBottomSheet(
               context: context,
               builder: (context) => Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Color(0xff4b4b4b),
+                ),
                 padding: EdgeInsets.symmetric(
                   horizontal: 10,
                   vertical: 15,
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Add Note",
-                      style: TextStyle(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          fillColor: Color(0xffc0c0c0),
-                          filled: true,
-                          hintText: "Titile"),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          fillColor: Color(0xffc0c0c0),
-                          filled: true,
-                          hintText: "Description"),
-                      maxLines: 4,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          suffixIcon: Icon(Icons.calendar_month),
-                          border: OutlineInputBorder(),
-                          fillColor: Color(0xffc0c0c0),
-                          filled: true,
-                          hintText: "Date"),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: SingleChildScrollView(
+                    child: Column(
                       children: [
-                        Container(
-                          child: Text(
-                            "Add",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600),
-                          ),
+                        Text(
+                          "Add Note",
+                          style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
                         ),
-                        Container(
-                          child: Text(
-                            "Cancel",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.red),
-                          ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Titile"),
                         ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Description"),
+                          maxLines: 4,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              suffixIcon: Icon(Icons.calendar_month),
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Date"),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: List.generate(
+                              4,
+                              (index) => Container(
+                                    height: 50,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.white),
+                                  )),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 10),
+                              child: Center(
+                                child: Text(
+                                  "Add",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.green),
+                                ),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white,
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Cancel",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.red),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
             );
           },
           child: Icon(Icons.add),
         ),
-        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.black,
         body: ListView.builder(
           itemCount: 10,
