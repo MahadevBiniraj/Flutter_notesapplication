d6995ea6f9d6f959d4c2b0bc4c0ad4abfc1305e7
MahadevBiniraj <mahadevbiniraj6@gmail.com>
2024-03-30 23:41:01 +0530

    core

---
+++ lib/core/Color_constants/ColorConstants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Colorconstants {
+  static const Color c1 = Color(0xffD20062);
+  static const Color c2 = Color(0xffD6589F);
+  static const Color c3 = Color(0xffD895DA);
+  static const Color c4 = Color(0xffC4E4FF);
+}
